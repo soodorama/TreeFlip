260c7bb4c59d755caacbaa87e5ec9b19594d59a3
Neil Sood <soodorama@gmail.com>
2018-09-30 01:13:41 -0700

    temp changes

--- TreeFlip/CauseVC.swift
+++ TreeFlip/CauseVC.swift
@@ -17,7 +17,7 @@ class CauseVC: UIViewController {
     
     @IBOutlet weak var percentTextField: UITextField!
     @IBOutlet weak var causeLabel: UITextField!
-    @IBOutlet var oppPercentLabel: UIView!
+    @IBOutlet weak var oppPercentLabel: UILabel!
     @IBOutlet weak var oppCauseLabel: UITextField!
     
     var delegate: CauseVCDelegate?
@@ -25,6 +25,14 @@ class CauseVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 //        percentTextField.keyboardType = UIKeyboardType.decimalPad
+        percentTextField.addTarget(self, action: #selector(CauseVC.textFieldDidChange(_:)), for: .editingChanged)
+
+    }
+    
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        print(textField)
+        
+        oppPercentLabel.text = 1.0 - Double(percentTextField.text)
     }
 
     @IBAction func forwardToEffectPressed(_ sender: UIBarButtonItem) {
